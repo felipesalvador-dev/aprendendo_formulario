1d8dcb76a9ae32be171b8e6539dceca73f3f6231
felipesalvador-dev <felipe.salvador8011@gmail.com>
2025-11-05 16:56:02 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const cors = require("cors")
+const app = express();
+const port = 3000;
+app.listen(port,()=>{
+    console.log(`Conexão realizada com sucesso na porta ${port}!! ✅`)
+})
